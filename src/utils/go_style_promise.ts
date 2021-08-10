9ccc1757060df7e10ba71fa727883dce292074f7
Yulun Wang <yulunwan@usc.edu>
2021-08-09 20:14:15 -0700

    Add eslint comments to disable warning in specific lines.
    Change type for goify to indicate the return must have one of the types as not undefined

--- src/utils/go_style_promise.ts
+++ src/utils/go_style_promise.ts
@@ -1,18 +1,23 @@
-// Had to use any to insure the typescript infers types correctly
+// For types in the file, there is no choice but using any to insure the typescript infers types correctly
+
+/* eslint-disable @typescript-eslint/no-explicit-any */
 type UnwrapPromise<T extends Promise<any>> = T extends Promise<infer RT>
     ? RT
     : never;
+/* eslint-enable @typescript-eslint/no-explicit-any */
 
 // TODO: Constrain return variables so one and only one will be undefined
 // TODO: Simply return type so it is less confusing while keep inferring working
-function goify<T extends (...args: any[]) => Promise<any>>(
+function goify<T extends (...args: any[]) => Promise<any>>( // eslint-disable-line @typescript-eslint/no-explicit-any
     promiseFunc: T,
 ): (
     ...args: Parameters<T>
-) => Promise<[UnwrapPromise<ReturnType<T>>?, Error?]> {
+) => Promise<[UnwrapPromise<ReturnType<T>>, undefined] | [undefined, Error]> {
     return async (
         ...args: Parameters<T>
-    ): Promise<[UnwrapPromise<ReturnType<T>>?, Error?]> => {
+    ): Promise<
+        [UnwrapPromise<ReturnType<T>>, undefined] | [undefined, Error]
+    > => {
         try {
             const result = await promiseFunc(...args);
             return [result, undefined];
@@ -22,7 +27,7 @@ function goify<T extends (...args: any[]) => Promise<any>>(
     };
 }
 
-export async function goCatch<T extends Promise<any>>(
+export async function goCatch<T extends Promise<any>>( // eslint-disable-line @typescript-eslint/no-explicit-any
     promise: T,
 ): Promise<[UnwrapPromise<T>?, Error?]> {
     try {
